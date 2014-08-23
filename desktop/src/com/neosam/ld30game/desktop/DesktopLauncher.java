7d4b3c57617b973d4d872152659454edcd6c15ab
Simon Goller <neosam@gmail.com>
2014-08-23 08:04:46 +0200

    Initial commit, default LibGDX startup

---
+++ desktop/src/com/neosam/ld30game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.neosam.ld30game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.neosam.ld30game.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
