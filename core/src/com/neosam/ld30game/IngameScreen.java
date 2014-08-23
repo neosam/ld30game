e552f4a081e45349963f89be05ab790490cf8a08
Simon Goller <neosam@gmail.com>
2014-08-23 08:19:15 +0200

    Basic box2d physics setup.

--- core/src/com/neosam/ld30game/IngameScreen.java
+++ core/src/com/neosam/ld30game/IngameScreen.java
@@ -3,6 +3,9 @@ package com.neosam.ld30game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -17,8 +20,17 @@ public class IngameScreen implements Screen {
     private Stage stage;
     private OrthographicCamera camera;
 
+    private World world;
+    private Box2DDebugRenderer debugRenderer;
+
     public IngameScreen() {
         initializeEssencials();
+        initializePhysics();
+    }
+
+    private void initializePhysics() {
+        world = new World(new Vector2(0, 0), true);
+        debugRenderer = new Box2DDebugRenderer();
     }
 
     private void initializeEssencials() {
@@ -30,10 +42,12 @@ public class IngameScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(Gdx.gl20.GL_COLOR_BUFFER_BIT);
+        world.step(delta, 2, 6);
         stage.act(delta);
         stage.draw();
+        debugRenderer.render(world, camera.combined);
     }
 
     @Override
