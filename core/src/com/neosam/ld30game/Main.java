b65e9a8f61ecdabcafb9eb4b2c7e3fa85d015827
Simon Goller <neosam@gmail.com>
2014-08-24 15:24:02 +0200

    Storyline completed.
    Needs improvements.

--- core/src/com/neosam/ld30game/Main.java
+++ core/src/com/neosam/ld30game/Main.java
@@ -11,13 +11,38 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Main extends Game implements IngameScreenListener {
     private Screen newScreenOnNewAct = null;
+    private int state = 0;
 
     @Override
     public void create() {
-        loadLevel1();
+        loadStory1();
+    }
+
+    public void loadLevel0() {
+        final IngameScreenDef ingameScreenDef = new IngameScreenDef();
+        ingameScreenDef.background1 = "background2.png";
+        ingameScreenDef.background2 = "background2.png";
+        ingameScreenDef.map1 = "intro.tmx";
+        ingameScreenDef.map2 = "intro.tmx";
+        ingameScreenDef.map2Offset = new Vector2(200, 0);
+        ingameScreenDef.background2Offset = new Vector2(-30, 0);
+        ingameScreenDef.ingameScreenListener = this;
+        newScreenOnNewAct = new IngameScreen(ingameScreenDef);
     }
 
     public void loadLevel1() {
+        final IngameScreenDef ingameScreenDef = new IngameScreenDef();
+        ingameScreenDef.background1 = "background2.png";
+        ingameScreenDef.background2 = "background.png";
+        ingameScreenDef.map1 = "level1-world1.tmx";
+        ingameScreenDef.map2 = "level1-world2.tmx";
+        ingameScreenDef.map2Offset = new Vector2(200, 0);
+        ingameScreenDef.background2Offset = new Vector2(-30, 0);
+        ingameScreenDef.ingameScreenListener = this;
+        newScreenOnNewAct = new IngameScreen(ingameScreenDef);
+    }
+
+    public void loadLevel2() {
         final IngameScreenDef ingameScreenDef = new IngameScreenDef();
         ingameScreenDef.background1 = "background2.png";
         ingameScreenDef.background2 = "background.png";
@@ -27,6 +52,68 @@ public class Main extends Game implements IngameScreenListener {
         ingameScreenDef.background2Offset = new Vector2(-30, 0);
         ingameScreenDef.ingameScreenListener = this;
         newScreenOnNewAct = new IngameScreen(ingameScreenDef);
+    }
+
+    public void loadStory1() {
+        final String[] images = {
+                "story/scene1/001.png",
+                "story/scene1/002.png",
+                "story/scene1/003.png",
+                "story/scene1/004.png"
+        };
+        newScreenOnNewAct = new StoryScreen(images, this);
+    }
+
+    public void loadStory2() {
+        final String[] images = {
+                "story/scene2/001.png",
+                "story/scene2/002.png",
+                "story/scene2/003.png",
+                "story/scene2/004.png",
+                "story/scene2/005.5.png",
+                "story/scene2/005.png",
+                "story/scene2/006.png",
+                "story/scene2/007.png",
+                "story/scene2/008.png",
+                "story/scene2/009.png",
+                "story/scene2/010.png",
+                "story/scene2/011.png",
+                "story/scene2/012.png",
+                "story/scene2/014.png",
+                "story/scene2/015.png",
+                "story/scene2/016.png",
+                "story/scene2/017.5.png",
+                "story/scene2/011.png",
+                "story/scene2/012.png",
+                "story/scene2/017.png",
+                "story/scene2/018.5.png",
+                "story/scene2/018.png",
+                "story/scene2/019.5.png",
+                "story/scene2/019.png",
+        };
+        newScreenOnNewAct = new StoryScreen(images, this);
+    }
+
+    public void loadStory3() {
+        final String[] images = {
+                "story/scene3/001.png",
+                "story/scene3/002.png",
+        };
+        newScreenOnNewAct = new StoryScreen(images, this);
+    }
+
+    public void loadStory4() {
+        final String[] images = {
+                "story/scene4/001.png",
+                "story/scene4/002.png",
+                "story/scene4/003.png",
+                "story/scene4/004.png",
+                "story/scene4/005.png",
+                "story/scene4/006.png",
+                "story/scene4/007.png",
+                "story/scene4/008.png",
+        };
+        newScreenOnNewAct = new StoryScreen(images, this);
     }
 
     @Override
@@ -44,6 +131,33 @@ public class Main extends Game implements IngameScreenListener {
 
     @Override
     public void finished() {
-        loadLevel1();
+        switch (state) {
+            case 0:
+                state = 10;
+                loadLevel0();
+                break;
+            case 10:
+                state = 20;
+                loadStory2();
+                break;
+            case 20:
+                state = 30;
+                loadLevel1();
+                break;
+            case 30:
+                state = 40;
+                loadStory3();
+                break;
+            case 40:
+                state = 50;
+                loadLevel2();
+                break;
+            case 50:
+                state = 60;
+                loadStory4();
+                break;
+            default:
+                System.exit(0);
+        }
     }
 }
---
+++ core/src/com/neosam/ld30game/StoryScreen.java
@@ -0,0 +1,133 @@
+package com.neosam.ld30game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+/**
+ * Created by neosam on 24.08.14.
+ */
+public class StoryScreen implements Screen, InputProcessor {
+    private final String[] imageNames;
+    private IngameScreenListener ingameScreenListener;
+    private Stage stage;
+    private int currentImage = -1;
+    private Texture currentTexture;
+
+    public StoryScreen(String[] imageNames, IngameScreenListener ingameScreenListener) {
+        this.imageNames = imageNames;
+        this.ingameScreenListener = ingameScreenListener;
+        final Viewport viewport = new ExtendViewport(640, 680);
+        stage = new Stage();
+        nextFrame();
+        Gdx.input.setInputProcessor(this);
+    }
+
+
+    @Override
+    public void render(float delta) {
+        final Batch batch = stage.getSpriteBatch();
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(Gdx.gl20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        if (currentTexture != null) {
+            batch.draw(currentTexture, 0, 0, 640, 480);
+        }
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+
+    private void nextFrame() {
+        currentImage++;
+        if (currentTexture != null) {
+            currentTexture.dispose();
+        }
+        if (currentImage >= imageNames.length) {
+            ingameScreenListener.finished();
+            return;
+        }
+        final AssetManager assetManager = new AssetManager();
+        assetManager.load(imageNames[currentImage], Texture.class);
+        assetManager.finishLoading();
+        currentTexture = assetManager.get(imageNames[currentImage], Texture.class);
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        nextFrame();
+        return true;
+
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+}
